412f6dbdba426f9edf6983237fb968a300dd9566
Producer234 <prexplanation2@gmail.com>
2025-10-18 16:05:45 +0600

    Clean up copyright and ensure event loop exists
    Removed copyright comments and added event loop check.

--- AlexaMusic/__init__.py
+++ AlexaMusic/__init__.py
@@ -1,14 +1,3 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
 import asyncio
 import sys
 
@@ -17,19 +6,22 @@ from AlexaMusic.core.dir import dirr
 from AlexaMusic.core.git import git
 from AlexaMusic.core.userbot import Userbot
 from AlexaMusic.misc import dbb, heroku
-
 from .logging import LOGGER
 
 
 if sys.platform != "win32":
     try:
         import uvloop
-
         asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
         LOGGER(__name__).info("Using Uvloop Event Loop for Enhanced Performance")
     except ImportError:
         LOGGER(__name__).warning("Uvloop not found, using default event loop.")
 
+# ✅ Ensure event loop exists before starting bots
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
 
 # Directories
 dirr()
